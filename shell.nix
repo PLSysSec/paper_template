86c8f4f9c3c77e8bc7da21c960d18c966e9be313
Peter Amidon <peter@picnicpark.org>
2019-06-26 15:31:51 -0700

    Simple shell.nix

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ nixpkgs ? import <nixpkgs> {} }: with nixpkgs;
+stdenv.mkDerivation {
+  name = "paper";
+  buildInputs = [ texlive.combined.scheme-full
+                  python3
+                  git
+                ];
+}
